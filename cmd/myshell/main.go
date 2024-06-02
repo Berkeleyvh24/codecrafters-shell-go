63e957389c8502267e455dd2b0d6edcb36f9111f
panman37 <baakejake@gmail.com>
2024-06-02 13:01:58 +0100

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -22,9 +22,26 @@ func main() {
 		commands := strings.Split(res, " ")
 		if commands[0] == "exit" {
 			os.Exit(0)
+
+		} else if commands[0] == "type" {
+			// cmd := exec.Command("type", strings.Join(commands[1:], " "))
+			// output, err := cmd.CombinedOutput()
+			// if err != nil {
+			// 	newString := string(output[:])
+			// 	stringArray := strings.Split(newString, " ")
+			// 	finalString := strings.Join(stringArray[4:], " ")
+			// 	fmt.Fprintf(os.Stdout, "%s", strings.Replace(finalString, ":", "", -1))
+			// } else {
+			// 	fmt.Printf("%s", output)
+			// }
+			switch commands[1] {
+			case "exit", "echo", "type":
+				fmt.Printf("%s is a shell builtin\n", commands[1])
+			default:
+				fmt.Printf("%s not found\n", commands[1])
+			}
 		} else if commands[0] == "echo" {
 			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(commands[1:], " "))
-
 		} else {
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", res)
 		}
